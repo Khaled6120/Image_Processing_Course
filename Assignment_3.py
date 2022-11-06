f9037c2db83c452d167bdc07614460370342df9f
Khaled6120 <86200305+Khaled6120@users.noreply.github.com>
2022-11-06 23:28:25 +0300

    Add files via upload

---
+++ Assignment_3.py
@@ -0,0 +1,12 @@
+import numpy as np
+import cv2 as cv
+from matplotlib import pyplot as plt
+img = cv.imread('figure1.png',0)
+hist,bins = np.histogram(img.flatten(),256,[0,256])
+cdf = hist.cumsum()
+cdf_normalized = cdf * float(hist.max()) / cdf.max()
+plt.plot(cdf_normalized, color = 'b')
+plt.hist(img.flatten(),256,[0,256], color = 'r')
+plt.xlim([0,256])
+plt.legend(('cdf','histogram'), loc = 'upper left')
+plt.show()
